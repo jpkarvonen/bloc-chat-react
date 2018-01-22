eaac4e8d77f12c2b5a6a53c3e115377f400a83c0
John <jpkarvonen@gmail.com>
2018-01-22 17:42:34 -0500

    add ability to delete + rename rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,6 +7,7 @@ class RoomList extends Component {
     this.state = {
       rooms: [],
       newRoomName: '',
+      changedRoomName: '',
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -27,8 +28,30 @@ class RoomList extends Component {
     this.setState({newRoomName: ''});
   }
 
+  deleteRoom(activeRoom) {
+    if (this.props.activeRoomKey === 'none') { return alert("Please select a room first."); }
+    this.roomsRef.child(activeRoom).remove();
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) });
+    });
+  }
+
+  changeRoomName(activeRoom, e) {
+    e.preventDefault();
+    if (this.props.activeRoomKey === 'none') { return alert("Please select a room first."); }
+    this.roomsRef.child(activeRoom).set({ 'name': this.state.changedRoomName });
+    this.setState({ changedRoomName: ''});
+  }
+
+
   handleChange(e) {
     this.setState( {newRoomName: e.target.value })
+  }
+
+  handleRoomNameChange(e) {
+    this.setState( {changedRoomName: e.target.value })
   }
 
   // Lifting State
@@ -46,7 +69,7 @@ class RoomList extends Component {
         </colgroup>
         <tbody>
           {this.state.rooms.map( (room, index) =>
-            <tr className="room" key={index} onClick ={(e) => this.handleRoomClick(room)}>
+            <tr className="room" key={index} onClick ={ (e) => this.handleRoomClick(room) }>
               <td className="room-number">{index + 1}</td>
               <td className="room-name">{room.name}</td>
             </tr>
@@ -57,7 +80,14 @@ class RoomList extends Component {
         <input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
         <input type="submit" value="Create a Room" />
       </form>
-    </div>
+      <button className="delete-room" onClick ={ (e) => this.deleteRoom(this.props.activeRoomKey) }>
+        <span> Delete Current Room </span>
+      </button>
+      <form className="change-room-name" onSubmit={ (e) => this.changeRoomName(this.props.activeRoomKey, e) }>
+        <input type="text" value={ this.state.changedRoomName } onChange={ (e) => this.handleRoomNameChange(e) } />
+        <input type="submit" value="Change Current Room Name" />
+      </form>
+      </div>
     );
   }
 
